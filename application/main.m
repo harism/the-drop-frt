aa9e742189218f09e7e4b84d74c52ec82af9bb97
Harri Smått <harism@gmail.com>
2020-08-01 11:45:21 +0300

    Initial commit

---
+++ application/main.m
@@ -0,0 +1,11 @@
+#import <Cocoa/Cocoa.h>
+//#import "appdelegate.h"
+
+int main(int arc, char** argv)
+{
+//    AppDelegate* appDelegate = [[AppDelegate alloc] init];
+//    [NSApplication sharedApplication];
+//    [NSApp setDelegate:appDelegate];
+//    [appDelegate release];
+    [NSApp run];
+}
---
+++ genxcode.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p xcode
+cd xcode
+cmake -G Xcode ..
